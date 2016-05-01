d4d00211f0f4261b1043b04e6aef03c2cba70910
owenmather <owenmather7@gmail.com>
2016-05-01 16:22:45 +0100

    Missed controler

---
+++ app/scripts/controllers/zombieKongController.js
@@ -0,0 +1,13 @@
+(function(){
+    
+    angular.module("zombieKong")
+    .controller("zombieKongController", zombieKongController);
+    
+    zombieKongController.$inject=["$scope","screenService"];
+    
+    function zombieKongController($scope,screenService){
+        
+    }//endOfZombieKongController
+    
+    
+})();
