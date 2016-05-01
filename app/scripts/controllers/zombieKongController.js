89a2d185c0f5e943a6d9038166bdf488d98ea4a9
owenmather <owenmather7@gmail.com>
2016-05-01 19:02:21 +0100

    Adding SplashScreen and preload

--- app/scripts/controllers/zombieKongController.js
+++ app/scripts/controllers/zombieKongController.js
@@ -15,17 +15,72 @@
        canvas =document.getElementById("demoCanvas");
        $("#main").width(cWidth).height(cHeight);
        $("#demoCanvas").width(cWidth).height(cHeight);
-       canvas = screenService.setCanvasDPI(canvas,cWidth,cHeight);
+       canvas = screenService.setCanvasDPI(canvas,cWidth,cHeight);//set appropiate canvas resolution
        context =canvas.getContext("2d");
        stage = new createjs.Stage("demoCanvas");  
        createjs.Ticker.setFPS(fps); //set our frameRate;
        createjs.Ticker.addEventListener("tick", render);//call our render loop on each tick
+       setupManifest();//set up our assets array
+       startPreload();//init preload queue
+       
+       canvas.style.backgroundColor = 'rgba(0, 0, 0, 1)';
     }
     
-    render = function(event){//render loop
-        
+    render = function(event){//render loop call stage.update when we want to show changes.
+        if(!pauseRendering){ 
+            stage.update();
+        }
     }
     
+    runMenuScreen=function(){
+        stage.removeChild(SplashLogo);
+        canvas.style.backgroundColor = 'rgba(223, 244, 215, 1)';
+    }
+    
+    
+    runSplashscreen =function(){//display Splashscreen. PreloadJs to load game assets
+            createjs.Tween.get(SplashLogo, { loop: false })
+            .to({ y:((cHeight-SplashLogo.getBounds().height)/2)}, 2000, createjs.Ease.getPowInOut(4))
+            .to({ alpha:0, visible:false}, 2000)//move to center then alpha out
+            .call(runMenuScreen);  
+    }
+    
+    loadComplete =function(){   
+        //stage.update();
+        pauseRendering=false;
+    }
+    
+    handleFileLoad = function(event){
+        if(event.item.id="SplashLogo"){
+            SplashLogo=new createjs.Bitmap(event.result); 
+            SplashLogo.x = ((cWidth-SplashLogo.getBounds().width)/2)//position our logo centre x
+            stage.addChild(SplashLogo);
+            runSplashscreen();
+        }//end if
+    }
+    
+    function setupManifest() {//code from http://code.tutsplus.com/tutorials/using-createjs-preloadjs-soundjs-and-tweenjs--net-36292
+        manifest = [{
+            src:  "images/SplashLogoFull.png",
+            id: "SplashLogo"
+        }
+        ];
+    }
+       
+    startPreload = function() {//init our preloadQueue. callback functions can be reenabled if needed and appropiate
+        preload = new createjs.LoadQueue(true);
+        preload.installPlugin(createjs.Sound);          
+        preload.on("fileload", handleFileLoad);
+        //preload.on("progress", handleFileProgress);
+        preload.on("complete", loadComplete);
+        //preload.on("error", loadError);
+        preload.loadManifest(manifest);
+ 
+    }
+    
+    var manifest; //a list of our assets to load
+    var preload; //preloadJs queue which we will init with our assets in manifest
+    var pauseRendering = true; //we will use this in render loop if needed to pause rendering for asset loading
     var context; //we will use 2d context for this game
     var canvas; //our html canvas object
     var stage; //our createJs stage
@@ -34,5 +89,5 @@
     var fps = 30;  //Frames Per Second. Lower this for analysis during development
     var player;
     var screenService; //Attach injectable screenService as a global lib
-    
+    var SplashLogo;
 })();
