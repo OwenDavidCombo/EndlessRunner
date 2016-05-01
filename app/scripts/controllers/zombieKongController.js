9e6c83999f7ee39707e2e54959d7d76447e8fb69
owenmather <owenmather7@gmail.com>
2016-05-01 17:15:16 +0100

    Setup init function

--- app/scripts/controllers/zombieKongController.js
+++ app/scripts/controllers/zombieKongController.js
@@ -10,16 +10,29 @@
         $scope.screenService=screenService;
     }//endOfZombieKongController
     
-    init = function(screenService){
+    init = function(screenService){//sets up the gameScreen and initialises 
        screenService=screenService;
+       canvas =document.getElementById("demoCanvas");
+       $("#main").width(cWidth).height(cHeight);
+       $("#demoCanvas").width(cWidth).height(cHeight);
+       canvas = screenService.setCanvasDPI(canvas,cWidth,cHeight);
+       context =canvas.getContext("2d");
+       stage = new createjs.Stage("demoCanvas");  
+       createjs.Ticker.setFPS(fps); //set our frameRate;
+       createjs.Ticker.addEventListener("tick", render);//call our render loop on each tick
     }
     
-    var canvas;
-    var stage;
-    var cWidth;
-    var cHeight;
-    var fps = 30;
+    render = function(event){//render loop
+        
+    }
+    
+    var context; //we will use 2d context for this game
+    var canvas; //our html canvas object
+    var stage; //our createJs stage
+    var cWidth=800;
+    var cHeight=600; //using 800*600. to see appropiate scaling use same aspect ratio 4:3 when increasing sizes
+    var fps = 30;  //Frames Per Second. Lower this for analysis during development
     var player;
-    var screenService;
+    var screenService; //Attach injectable screenService as a global lib
     
 })();
